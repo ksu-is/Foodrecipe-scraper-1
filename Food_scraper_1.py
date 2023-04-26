19ecd9fa627f9dbcb574e2856b37236747dda293
nik348 <122301690+nik348@users.noreply.github.com>
2023-04-26 18:43:40 +0000

    after reviewing the error, i realized that I had to ad an underscore after the word class, due to "class" being a reserved word in python.

--- Food_scraper_1.py
+++ Food_scraper_1.py
@@ -19,7 +19,7 @@ recipe_links = soup.find_all("a", class_="o-ResultCard__a-Anchor")
 for link in recipe_links:
     recipe_url = link["href"]
     recipe_name = link.find("h3", class_="m-MediaBlock__a-HeadlineText").get_text()
-    recipe_cuisine = link.find("span", class="o-RecipeInfo__a-Category").get_text()
+    recipe_cuisine = link.find("span", class_="o-RecipeInfo__a-Category").get_text()
 
     # Print out the recipe name and URL
     print(recipe_name)
