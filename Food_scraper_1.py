3ac7609a4c6441095e2b789042d493d9cab803ce
Bunka4 <scrary2@students.kennesaw.edu>
2023-04-26 16:22:59 -0400

    this portion i added a "while true" statment

--- Food_scraper_1.py
+++ Food_scraper_1.py
@@ -5,26 +5,21 @@ This is a scraping tool, intended to help people decide and cook what's for dinn
 import requests
 from bs4 import BeautifulSoup
 
-# Send an HTTP request to the URL
-url = "https://www.foodnetwork.com/recipes"
-response = requests.get(url)
+while True: 
+    # Prompt user to input name 
+    recipe_name = input("Ente a recipe name: ")
+    
+    # Construct URL witha search query
+    url = f"https://www.foodnetwork.com/search/{recipe_name}-CUSTOM_FACET:RECIPE_FACET"
+    response = requests.get(url)
+    
+    #Parse the HTML content using Beautiful soup
+    soup = BeautifulSoup(response.content, "html.parser")
+    
+    # Find the first search result link
+    search_result = soup.find("a", class_="m-MediaBlock_a-headlineAnchor")
 
-# Parse the HTML content using Beautiful Soup
-soup = BeautifulSoup(response.content, "html.parser")
-
-# Find all recipe links on the page
-recipe_links = soup.find_all("a", class_="o-ResultCard__a-Anchor")
-
-# Extract information from each recipe link
-for link in recipe_links:
-    recipe_url = link["href"]
-    recipe_name = link.find("h3", class_="m-MediaBlock__a-HeadlineText").get_text()
-    recipe_cuisine = link.find("span", class_="o-RecipeInfo__a-Category").get_text()
-
-    # Print out the recipe name and URL
-    print(recipe_name)
-    print(recipe_url)
-
-    # Print a user with a good by method
-    print("Thank you for using this tools, bye bye")
+    if search_result is None:
+        print("No results found. Please try again.")
+        continue
     
